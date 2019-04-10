f78777bf555409339b3ed6a694f9d747c72006aa
Soumik De <47034865+smk612@users.noreply.github.com>
2019-04-10 09:02:32 +0530

    Add files via upload

---
+++ Bachelors/Sem VI/Socket Programming/5/client.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define BUFSIZE 1024
+
+main()
+{
+	int clientSock;
+	struct sockaddr_in serverAddr;
+	socklen_t addr_size;
+	char server_ip[] = "127.0.0.1";
+	unsigned short server_port=25051;
+	char sendBuf[BUFSIZE], recvBuf[BUFSIZE];
+	printf("\n\tEnter Sentence:\n\t");
+	gets(sendBuf);
+	bzero(&serverAddr,sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;//Internet Address family
+	serverAddr.sin_port = htons(server_port);//Local Port address
+	inet_aton(server_ip,(&serverAddr.sin_addr));
+	if((clientSock=socket(AF_INET,SOCK_DGRAM,0))<0)
+	{
+		printf("\n\tSocket Error.\n");
+		exit(1);
+	}
+	printf("\n\tCLIENT: Socket Created.\n");
+	addr_size=sizeof(struct sockaddr_in);
+	if(sendto(clientSock,sendBuf,sizeof(sendBuf),0,(struct sockaddr *)&serverAddr,addr_size)<0)
+	{
+		printf("\n\tSend Error.\n");
+		exit(1);
+	}
+	printf("\n\tCLIENT: Sent.\n");
+	close(clientSock);
+}
